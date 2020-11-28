6503f5b8cd015ba382a9b1507c5feb9a5c5a8e16
jadealmeida83 <71714811+jadealmeida83@users.noreply.github.com>
2020-11-27 23:15:36 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# atividadefinal
+# Model View Controller - Design Pattern para PHP
+
+## Modelos
+
+Todas as funções do banco de dados são armazenadas aqui.
+Cada tabela tem seu próprio objeto dentro da pasta do modelo.
+
+## Visualizações
+
+Todas as páginas da web são armazenadas aqui.
+Cada página tem sua própria pasta com um index.php.
+Todos os dados são passados para a visualização por meio da variável \$ data.
+
+## Controller
+
+Todas as funcionalidades são armazenadas aqui.
+Todos os modelos são chamados dentro dos controladores e podem ser modificados e depois passados para a visualização.
---
+++ app/views/home/cabecario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Aula programacao web</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet"
+        type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+
+</head>
+
+<body id="page-top">
